1fa814d06351cebdf372312fb5ab1e90f6872d20
Daniel Felin <felinamaral@gmail.com>
2023-08-22 01:08:05 -0300

    .

---
+++ test03.py
@@ -0,0 +1,4 @@
+dia = input('dia do seu nacimento: ')
+mes = input('mes do seu nascimento: ')
+ano = input('ano do seu nascimento: ')
+print('Voce nasceu no dia ', dia, ' do ', mes, ' de ', ano )
---
+++ test04.py
@@ -0,0 +1,3 @@
+n1 = input('Digite um numero: ')
+n2 = input('Digite outro numero: ')
+print('A soma dos resultados é: ', n1 + n2)
